a4b26fc183796ddbaea3725fb15a4fc8151dad33
LJWstat <jiwoo04082500@gmail.com>
2025-11-14 10:46:09 +0900

    Initial commit

---
+++ R/고통분 과제1.R
@@ -0,0 +1,12 @@
+# library(usethis)
+# library(roxygen2)
+# library(knitr)
+# library(devtools)
+#
+# setwd("C:/Users/jiwoo/OneDrive - 성신여자대학교/문서/")
+# devtools::create("asa.rosie") # description, namespace 등등이 자동생성
+
+getwd()
+usethis::use_git()
+
+usethis::use_github()
